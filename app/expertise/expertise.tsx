a7681cdc0d5e7e0422371e4f819e6915673f865c
Ameer Ul Aman Jodad Sultan <w.ameerulaman@gmail.com>
2024-07-10 22:51:57 +0500

    Header changes

--- app/expertise/expertise.tsx
+++ app/expertise/expertise.tsx
@@ -8,46 +8,46 @@ const Expertise = () => {
     {
       title: 'Web GIS Development',
       content: 'GIS web development combines GIS with web technologies to create interactive maps and data visualizations.',
-      bgColor: '#ffffff',
+    
       image: '/ex1.jpg',
     },
     {
       title: 'GIS Consultancy',
       content: 'We provide expert guidance and support for clients in leveraging GIS technology for their business needs.',
-      bgColor: '#ffffff',
+     
       image: '/ex2.jpg',
     },
     {
       title: 'Mobile GIS App Development',
       content: 'We create such apps that allow users to access and analyze geographic data on mobile devices.',
-      bgColor: '#ffffff',
+     
       image: '/ex3.jpg',
       },
     {
       title: 'Automation',
       content: 'We provide cutting-edge automation solutions that improve efficiency and productivity in a variety of industries.',
-      bgColor: '#ffffff',
+    
       image: '/ex4.jpg',
      },
     {
       title: 'GIS Analysis',
       content: 'We offers comprehensive GIS analysis services to help clients gain insights and make informed decisions about spatial data',
-      bgColor: '#ffffff',
+     
       image: '/ex5.jpg',
     },
     { title: 'Integrations',
       content: 'We provide expert integration services to help clients seamlessly integrate GIS technology with their existing systems and workflows.',
-      bgColor: '#ffffff',
+    
       image: '/ex6.jpg',
     },
     { title: 'Database Design',
       content: 'We offer professional database design services to help clients create efficient and scalable data management solutions',
-      bgColor: '#ffffff',
+      
       image: '/ex7.jpg',
     },
     {title: 'Remote Sensing',
       content: "We provide advanced remote sensing solutions for clients to gather and analyze data about the Earth's surface from afar.",
-      bgColor: '#ffffff',
+      
       image: '/ex8.jpg',
     }
   ];
--- app/intro/intro.tsx
+++ app/intro/intro.tsx
@@ -1,30 +1,50 @@
-import styles from './intro.module.css';
-import React from 'react'
+"use client";
 
-const Intro = () => {
+import styles from './intro.module.css';
+import React, { useEffect } from 'react';
+
+const Intro: React.FC = () => {
+  useEffect(() => {
+    generateStars(50);
+  }, []);
+
+  const generateStars = (starCount: number) => {
+    const starfield = document.querySelector(`.${styles.starfield}`);
+    if (starfield) {
+      for (let i = 0; i < starCount; i++) {
+        const star = document.createElement('div');
+        star.classList.add(styles.stars);
+        star.style.top = `${Math.random() * 100}%`;
+        star.style.left = `${Math.random() * 100}%`;
+        star.style.animationDelay = `${Math.random() * 2}s`;
+        starfield.appendChild(star);
+      }
+    }
+  };
+
   return (
     <>
-<div className={styles.dif}></div>
-    <div className={styles.frex}>
-    <div className={styles.introtext}>
-        <h1>GIS Solutions & Services</h1>
-        <p>Jugrafiya specializes in providing top-tier Web GIS Development services to businesses. Based in Pakistan, our team of highly experienced GIS Engineers specializes in:</p>
-<ul>
-<li>Open-Source GIS Solutions</li>
-<li>Web & Mobile Development</li>
-<li>Complete ArcGIS Suite development</li>
-<li>Automation pipelines</li>
-</ul>
-<button className={styles.but}>Explore</button>
-<button className={styles.but}>Get in touch</button>
-    </div>
-    <div className={styles.introtext}>
-        <img src="sat1.png" alt="SATELLITE" />
-    </div>
-    </div>
-    
+      <div className={styles.dif}></div>
+      <div className={styles.frex}>
+        <div className={styles.introtexta}>
+       <center><h1>GIS Solutions & Services</h1></center>   
+          <p>Jugrafiya specializes in providing top-tier Web GIS Development services to businesses. Based in Pakistan, our team of highly experienced GIS Engineers specializes in:</p>
+          <ul>
+            <li>Open-Source GIS Solutions</li>
+            <li>Web & Mobile Development</li>
+            <li>Complete ArcGIS Suite development</li>
+            <li>Automation pipelines</li>
+          </ul>
+          <button className={styles.but}>Explore</button>
+          <button className={styles.but}>Get in touch</button>
+        </div>
+        <div className={styles.introtext}>
+          <div className={styles.starfield}></div> {/* Container for stars */}
+          <img src="sat1.png" alt="SATELLITE" />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
